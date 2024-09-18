92adfdc5d61b94cea42ddfd6a448d12dbe2b8919
Sam Wells <swells1@mines.edu>
2024-09-18 12:46:46 -0600

    Changed to non-movable Lilypad

---
+++ levelPieces/Lilypad.java
@@ -0,0 +1,17 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Lilypad implements Drawable{
+	private int loc;
+  private char sym;
+  private String lab;
+	public Lilypad(char symbol, String label, int location) {
+	  	loc = location;
+      sym = symbol;
+      lab = label;
+  }
+
+
+}
