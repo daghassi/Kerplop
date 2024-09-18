5b3aa0804db11ffb628b4a0a48eaf336bcd3da02
Sam Wells <swells1@mines.edu>
2024-09-18 13:42:19 -0600

    Added player7

---
+++ levelPieces/Player7.java
@@ -0,0 +1,20 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Player6 extends GamePiece{
+		private int loc;
+	public Player6(char symbol, String label, int location) {
+		super(symbol, label, location);
+		// TODO Auto-generated constructor stub
+		loc = getLocation();
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
