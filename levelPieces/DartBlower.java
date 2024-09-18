fb47a4ee19f77e3c48b4bd271ed360133f768f71
Sam Wells <swells1@mines.edu>
2024-09-18 13:38:34 -0600

    Added dartblower

---
+++ levelPieces/DartBlower.java
@@ -0,0 +1,25 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class DartBlower extends GamePiece{
+	private int loc;
+  private char sym;
+  private String lab;
+	public DartBlower(char symbol, String label, int location) {
+		super(symbol, label, location);
+		sym = symbol;
+    lab = label;
+		loc = getLocation();
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		if (player.getLocation() < loc + 2 || player.getLocation > loc - 2) {
+      return InteractionResult.KILL;
+    }
+		return null;
+	}
+	
+}
