16af50054d4ea748d37822280df5fb48b9146ecf
Sam Wells <swells1@mines.edu>
2024-09-23 00:57:29 -0600

    Added frog test

--- tests/TestInteractions.java
+++ tests/TestInteractions.java
@@ -1,1 +1,21 @@
 package tests;
+
+public void birdTest() {
+  Bird bird = new Bird('B', "Bird", 5);
+  IneractionResult result = bird.interact(gameBoard, 5);
+  // Test for hitting player when at location
+  assertEquals(InteractionResult.HIT, result);
+
+  InteractionResult result2 = bird.interact(gameBoard, 3);
+  // Test for not hitting player when not at location
+  assertNull(result);
+}
+
+public void frogTest() {
+  Frog frog = new Frog('F', "Frog", 4);
+  InteractionResult result = frog.interact(gameBoard, 0);
+  // Test for lack of interaction
+  assertEquals(InteractionResult.NONE, result);
+}
+
+
